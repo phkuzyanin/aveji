b2c9ee1f35aa0872be3e5e32b2aae27e9cfdbd27
phkuzyanin <phkuzyanin@gmail.com>
2023-10-15 07:23:58 +0300

    end

--- js/main.js
+++ js/main.js
@@ -1,3 +1,29 @@
+const owl = $('.owl-carousel');
+owl.owlCarousel({
+  	center: false,
+  	loop: true,
+  	startPosition: 0,
+  	margin: 10,
+  	items: 1,
+  	responsive : {
+	    600: {
+	    	items: 3,
+	    },
+	    800:{
+	  		items: 3,
+	    },
+	    1000:{
+	    	items:4,
+	    },
+	}
+  });
+
+
+
+
+
+
+
 const navBtn = document.querySelector('.nav__toggle');
 const nav = document.querySelector('.header__links')
 const menuIcon = document.querySelector('.menu-icon');
